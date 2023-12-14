33ecc9b72a152af867d9c221ee511999abe4b4b2
Gonzalo Lopez Gaschetto <gonzalolg.lopez@gmail.com>
2023-12-14 16:24:26 -0300

    modificacion landinpage

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+const open = document.getElementById("menu_open1")
+const close = document.getElementById("menu_close2")
 
+let nav = document.getElementById("nav")
+ 
+open.addEventListener("click",() =>{
+    console.log("funciona")
+    nav.style.display = "block";
+    document.getElementById("menu_open1").style.display = "none";
+    document.getElementById("menu_close2").style.display = "block";
+
+    
+})
+close.addEventListener("click",() =>{
+    console.log("funciona")
+    nav.style.display = "none";
+    document.getElementById("menu_open1").style.display = "block";
+    document.getElementById("menu_close2").style.display = "none";
+
+    
+})
 
