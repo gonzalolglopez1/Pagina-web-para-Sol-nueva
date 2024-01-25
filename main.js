79864a0ed74ecb7e6105c43a9a46d36eab1d150e
Gonzalo Lopez Gaschetto <gonzalolg.lopez@gmail.com>
2024-01-25 12:43:38 -0300

    agregado de version escritorio

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const open = document.getElementById("menu_open1")
 const close = document.getElementById("menu_close2")
 
+const inicio = document.getElementById("btninicio")
+const cartas = document.getElementById("btncartas")
+const cursos = document.getElementById("btncursos")
+const soy = document.getElementById("btnsoy")
+const blog = document.getElementById("btnblog")
+const contacto = document.getElementById("btncontacto")
+
+let menues = [inicio, cartas, cursos, soy, blog, contacto];
+
 let nav = document.getElementById("nav")
  
 open.addEventListener("click",() =>{
@@ -16,7 +25,15 @@ close.addEventListener("click",() =>{
     nav.style.display = "none";
     document.getElementById("menu_open1").style.display = "block";
     document.getElementById("menu_close2").style.display = "none";
-
+    
     
 })
 
+menues.forEach(element => {
+  element.addEventListener("click", () =>{
+    nav.style.display = "none";
+open.style.display = "block";
+close.style.display = "none";
+  }
+  )  
+});
